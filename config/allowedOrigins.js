1813e4a545283f37cc2b03d6b12444de998dcb27
MaslahGure <maslah.abdikarim97@gmail.com>
2022-09-03 17:24:09 +0800

    updated allowed cores

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
-    'https://insightdashboard.xyz/',
-    'http://insightdashboard.xyz/',
+    'https://insightdashboard.xyz',
+    'http://insightdashboard.xyz',
     'http://localhost:3001',
     'http://localhost:3000'
 ];
